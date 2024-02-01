519f72c8f29b774cf3ff6880203bccfc2a618466
Santiago Carmuega <santiago@carmuega.me>
2024-01-31 22:07:57 -0300

    fix: use interceptor to inject headers

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,39 @@
-import { PromiseClient, createPromiseClient } from "@connectrpc/connect";
+import {
+  Interceptor,
+  PromiseClient,
+  createPromiseClient,
+} from "@connectrpc/connect";
 import { createGrpcTransport } from "@connectrpc/connect-node";
 import { ChainSyncService } from "@utxorpc/sync-spec/utxorpc/sync/v1/sync_connect.js";
 
-export class CardanoClientV1Alpha {
+export type Options = {
   headers: Record<string, string>;
+};
+
+function buildHeaderInterceptor(options?: Options): Interceptor {
+  return (next) => async (req) => {
+    if (!!options?.headers) {
+      Object.entries(options.headers).forEach(([key, value]) =>
+        req.header.set(key, value)
+      );
+    }
+
+    return await next(req);
+  };
+}
+
+export class CardanoClientV1Alpha {
   chainSync: PromiseClient<typeof ChainSyncService>;
 
-  constructor(baseUrl: string, options?: { headers: Record<string, string> }) {
-    const transport = createGrpcTransport({ httpVersion: "2", baseUrl });
-    this.headers = options?.headers || {};
+  constructor(baseUrl: string, options?: Options) {
+    let headerInterceptor = buildHeaderInterceptor(options);
+
+    const transport = createGrpcTransport({
+      httpVersion: "2",
+      baseUrl,
+      interceptors: [headerInterceptor],
+    });
+
     this.chainSync = createPromiseClient(ChainSyncService, transport);
   }
 }
