a30cb6610eaa5dd91c9ae1d1419d8b2563dfe91d
Rico Miles Quiblat <120778516+ricomiles@users.noreply.github.com>
2025-01-03 17:20:31 +0800

    fix: export WatchClient (#31)

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export {
   SyncClient as CardanoSyncClient,
   QueryClient as CardanoQueryClient,
   SubmitClient as CardanoSubmitClient,
+  WatchClient as CardanoWatchClient,
 } from "./cardano.js";
