82611f93ce6381be0a1168c021cb584971192bc6
Sebastien Guillemot <SebastienGllmt@gmail.com>
2025-03-15 18:42:14 -0600

    fix: add missing exports with Cardano prefix #39

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,18 @@ export {
   QueryClient as CardanoQueryClient,
   SubmitClient as CardanoSubmitClient,
   WatchClient as CardanoWatchClient,
+  ChainPoint as CardanoChainPoint,
+  Utxo as CardanoUtxo,
+  TipEvent as CardanoTipEvent,
+  TxEvent as CardanoTxEvent,
+  MempoolEvent as CardanoMempoolEvent,
+  TxHash as CardanoTxHash,
+  TxCbor as CardanoTxCbor,
 } from "./cardano.js";
+export type {
+  ClientBuilderOptions,
+  GenericTipEvent,
+  GenericTxEvent,
+  GenericTxInMempoolEvent,
+  GenericUtxo,
+} from "./common.js";
